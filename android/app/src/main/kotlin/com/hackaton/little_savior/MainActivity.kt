03b4c2e375dfc6aec8538f7f74da9e77aaba7fb2
Ruben Martinez <rubenmartinezTRABALLOS@gmail.com>
2024-10-22 17:52:24 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/hackaton/little_savior/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hackaton.little_savior
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:little_savior/views/home_view.dart';
+import 'package:little_savior/views/splash_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Little Saviors',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromRGBO(254,	247,	215, 1),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
